428436ca526908f024578015f1221129debfd49f
RY4GIT <400mhs2@gmail.com>
2025-01-13 13:47:48 -0800

    improve: added more comments on the config generating file

--- random_forest/configs/generate_config_by_ecoregion.py
+++ random_forest/configs/generate_config_by_ecoregion.py
@@ -1,31 +1,16 @@
+# %%
 import yaml
 import os
 
+# %%
+#######################################################
+# START CONFIG
+# CHANGE HERE FOR YOUR PURPOSE
+#######################################################
 
-# Function to generate YAML files
-def generate_yaml_files(ecoregions, template_yaml, out_dir):
+# OS type
+os_type = "win"  # "linux" or "win"
 
-    for ecoregion in ecoregions:
-        # Create a copy of the template
-        yaml_content = template_yaml.copy()
-
-        # Modify the experiment_name and filter_by_ecoregion$name
-        ecoregion_cleaned = ecoregion.replace("  ", "_").replace(
-            " ", "_"
-        )  # Clean up for filename
-        ecoregion_num = ecoregion_cleaned.split("_")[0]
-        yaml_content["experiment_name"] = f"ecoregion_{ecoregion_num}"
-        yaml_content["filter_by_ecoregion"]["name"] = ecoregion
-
-        # Define the output filename
-        output_filename = f"config_ecoregion_{ecoregion_num}.yml"
-
-        # Write the YAML content to the file
-        with open(os.path.join(out_dir, output_filename), "w") as file:
-            yaml.dump(yaml_content, file, default_flow_style=False, sort_keys=False)
-
-
-os_type = "linux"  # "linux" or "win"
 # List of ecoregions
 ecoregions = [
     "11  MEDITERRANEAN CALIFORNIA",
@@ -37,6 +22,7 @@ ecoregions = [
     "92 South Great Plains",
 ]
 
+# Original EPA Level 1 ecoregion definition
 # ecoregions = [
 #     "5  NORTHERN FORESTS",
 #     "10  NORTH AMERICAN DESERTS",
@@ -49,6 +35,30 @@ ecoregions = [
 #     "9  GREAT PLAINS",
 # ]
 
+# Hammond et al., 2021 definition
+# ecoregions = [
+#     "8  EASTERN TEMPERATE FORESTS"
+#     "5  NORTHERN FORESTS"
+#     "91 North Great Plains"
+#     "92 South Great Plains"
+#     "613 Western Mountains"
+#     "1210 Western Deserts"
+#     "11  MEDITERRANEAN CALIFORNIA"
+#     "7  MARINE WEST COAST FOREST"
+# ]
+
+# Hammond et al., 2021 definition + further aggregating/splitting some regions
+# ecoregions = [
+#     "81 North Eastern Forests"
+#     "82 South Eastern Forests"
+#     "91 North Great Plains"
+#     "92 South Great Plains"
+#     "6713 Western Mountains"
+#     "1210 Western Deserts"
+#     "11  MEDITERRANEAN CALIFORNIA"
+# ]
+# %%
+# File path
 if os_type == "win":
     home_dir = "G:/Shared drives/Signatures -- large scale/baseflow"
     rf_out_dir = "RAraki/out/rf"
@@ -72,8 +82,25 @@ elif os_type == "linux":
     )
 if not os.path.exists(config_out_dir):
     os.makedirs(config_out_dir)
-
+# %%
+# ________________________________________________________________________________________________________
 # Template YAML content
+
+# ### Signatures to predict: we excluded signatures that are known to be unstable based on McMillan et al., 2021.
+# The full list of signature candidates & thought processes are saved in:
+# "G:\Shared drives\Signatures -- large scale\baseflow\RAraki\docs\sig_analysis.xlsx"
+# https://docs.google.com/spreadsheets/d/1zYj7LPZeTagrsIV9iXbIKLM4lpyjUPbT?rtpof=true&usp=drive_fs
+
+# ### Attributes to use: we excluded attributes that are
+# - Not landscape attributes (attributes of Hydrology group, and soil erosion rate)
+# - Categorical variables, following Annie’s analysis
+# - Monthly climate variables (e.g., average PET in January, February, …), because hard to choose the representative values & they need to be (ideally) represented by annual statistics
+# - Speaman’s corr  |$\rho$| > 0.8
+# - Results: 26 attributes survived
+# The full list of available Caravan attributes & thought processes are saved in:
+# "G:\Shared drives\Signatures -- large scale\baseflow\RAraki\docs\attrs_analysis.xlsx"
+# https://docs.google.com/spreadsheets/d/1eGk1OD4mCt3UmlyhUv27iXp5V0MfjZO0?rtpof=true&usp=drive_fs
+
 # Not sure why but "min_Qf_perc" doesn't work with Linux ...
 template_yaml = {
     "paths": {
@@ -142,16 +169,45 @@ template_yaml = {
         "low_prec_dur",
     ],
 }
+#######################################################
+# END CONFIG ######
+#######################################################
 
-
+# %%
+# ________________________________________________________________________________________________________
 # Generate the YAML files
+
 # ___________________________________________
 # By ecoregion
-generate_yaml_files(ecoregions, template_yaml, config_out_dir)
 
+
+# Function to generate YAML files
+def generate_yaml_files(ecoregions, template_yaml, out_dir):
+    for ecoregion in ecoregions:
+        # Create a copy of the template
+        yaml_content = template_yaml.copy()
+
+        # Modify the experiment_name and filter_by_ecoregion$name
+        ecoregion_cleaned = ecoregion.replace("  ", "_").replace(
+            " ", "_"
+        )  # Clean up for filename
+        ecoregion_num = ecoregion_cleaned.split("_")[0]
+        yaml_content["experiment_name"] = f"ecoregion_{ecoregion_num}"
+        yaml_content["filter_by_ecoregion"]["name"] = ecoregion
+
+        # Define the output filename
+        output_filename = f"config_ecoregion_{ecoregion_num}.yml"
+
+        # Write the YAML content to the file
+        with open(os.path.join(out_dir, output_filename), "w") as file:
+            yaml.dump(yaml_content, file, default_flow_style=False, sort_keys=False)
+
+
+generate_yaml_files(ecoregions, template_yaml, config_out_dir)
+# %%
 
 # ______________________
-# Export global one
+# Whole CONUS
 yaml_content = template_yaml.copy()
 
 # Modify the experiment_name and filter_by_ecoregion$name
@@ -168,3 +224,5 @@ with open(os.path.join(config_out_dir, output_filename), "w") as file:
 
 
 print(f"Output results to {config_out_dir}")
+
+# %%
