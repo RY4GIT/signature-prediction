9801fea4dd7c69a1ef64e267b202d0e9dab3318a
RY4GIT <400mhs2@gmail.com>
2025-02-20 17:34:54 -0800

    change RF config

--- random_forest/run.sh
+++ random_forest/run.sh
@@ -10,29 +10,51 @@
 project_dir="/home/raraki/signature-prediction/random_forest"
 config_dir="$project_dir/configs/linux"
 
+echo "Running experiment with $config_dir/config_gages2exp_baseline_allcaravan_withoutsnow.yml"
+Rscript main_mp.R "$config_dir/config_gages2exp_baseline_allcaravan_withoutsnow.yml"
+echo "Experiment with $config_dir/config_gages2exp_baseline_allcaravan_withoutsnow.yml finished"
+
+echo "Running experiment with $config_dir/config_gages2exp_baseline_withoutsnow.yml"
+Rscript main_mp.R "$config_dir/config_gages2exp_baseline_withoutsnow.yml"
+echo "Experiment with $config_dir/config_gages2exp_baseline_withoutsnow.yml finished"
+
+echo "Running experiment with $config_dir/config_gages2exp_gages2attrs_withoutsnow.yml"
+Rscript main_mp.R "$config_dir/config_gages2exp_gages2attrs_withoutsnow.yml"
+echo "Experiment with $config_dir/config_gages2exp_gages2attrs_withoutsnow.yml finished"
+
+echo "Running experiment with $config_dir/config_gages2exp_gages2attrs.yml"
+Rscript main_mp.R "$config_dir/config_gages2exp_gages2attrs.yml"
+echo "Experiment with $config_dir/config_gages2exp_gages2attrs.yml finished"
+
+echo "Running experiment with $config_dir/config_gages2exp_surfacewater.yml"
+Rscript main_mp.R "$config_dir/config_gages2exp_surfacewater.yml"
+echo "Experiment with $config_dir/config_gages2exp_surfacewater.yml finished"
+
+echo "Running experiment with $config_dir/config_gages2exp_bestattrs_and_surfacewater.yml"
+Rscript main_mp.R "$config_dir/config_gages2exp_bestattrs_and_surfacewater.yml"
+echo "Experiment with $config_dir/config_gages2exp_bestattrs_and_surfacewater.yml finished"
+
+
 # # For test
 # echo "Running experiment with $config_dir/config_test.yml"
 # Rscript main_mp.R "$config_dir/config_test.yml"
 # echo "Experiment with $config_dir/config_test.yml finished"
 
 # # List of ecoregion codes
-ecoregions="11 1210 6713 81 82 91 92"
+# ecoregions="11 1210 6713 81 82 91 92"
 
-# # Loop through each ecoregion code
-for i in $ecoregions
-do
-    echo "Running experiment with $config_dir/config_ecoregion_$i.yml"
-    Rscript main_mp.R "$config_dir/config_ecoregion_$i.yml"
-    echo "Experiment with $config_dir/config_ecoregion_$i.yml finished"
-done
+# # # Loop through each ecoregion code
+# for i in $ecoregions
+# do
+#     echo "Running experiment with $config_dir/config_ecoregion_$i.yml"
+#     Rscript main_mp.R "$config_dir/config_ecoregion_$i.yml"
+#     echo "Experiment with $config_dir/config_ecoregion_$i.yml finished"
+# done
 
 # echo "Running experiment with $config_dir/config_ecoregion_11.yml"
 # Rscript main_mp.R "$config_dir/config_ecoregion_11.yml"
 # echo "Experiment with $config_dir/config_ecoregion_11.yml finished"
 
-echo "Running experiment with $config_dir/config_caravan_us.yml"
-Rscript main_mp.R "$config_dir/config_caravan_us.yml"
-echo "Experiment with $config_dir/config_caravan_us.yml finished"
 
 # for i in {5..13}
 # do
