a449cf9068eaefbcdb346e1e9c5369f416fb0618
RY4GIT <400mhs2@gmail.com>
2025-06-02 21:46:48 -0700

    EOD

---
+++ data_mng/shp_mng/gages2_polygon_subset.py
@@ -0,0 +1,58 @@
+# %%
+import geopandas as gpd
+import os
+import pandas as pd
+
+# %%
+gages2_dir = r"G:\Shared drives\Signatures -- large scale\baseflow\RAraki\data\GAGES2\GAGES_II_Geospa"
+gages2_polygons = gpd.read_file(os.path.join(gages2_dir, "all_gages2_polygons.shp"))
+
+# %%
+gages2_polygons.columns
+# %%
+print(f"There are {len(gages2_polygons)} gages2 polygons")
+# %% Get the polygon subset that is NOT overlapping with the Caravan gages
+cara_attrs = r"G:\Shared drives\Signatures -- large scale\baseflow\RAraki\data\derived_attrs\assembled_RA\attrs_cara_gages2_etc_20250517+cluster.csv"
+cara_attrs = pd.read_csv(cara_attrs)
+cara_attrs["usgs_gauge_id"] = (
+    cara_attrs["gauge_id"].astype(str).str.split("_").str[1].astype(str).str.zfill(8)
+)
+print(f"There are {len(cara_attrs)} Caravan gages")
+
+cara_subset = cara_attrs[~cara_attrs["DRAIN_SQKM"].isna()]
+print(f"There are {len(cara_subset)} Caravan gages with no area")
+# %%
+gages2_polygons["usgs_gauge_id"] = gages2_polygons["GAGE_ID"].astype(str).str.zfill(8)
+
+# %%
+gages2_polygons_not_cara = gages2_polygons[
+    ~gages2_polygons["usgs_gauge_id"].isin(cara_subset["usgs_gauge_id"])
+]
+print(
+    f"There are {len(gages2_polygons_not_cara)} gages2 polygons that are not Caravan gages"
+)
+
+# Make sure its within the CONUS
+conus_bbox = (24.5, -124.7, 49.4, -66.9)
+length_before = len(gages2_polygons_not_cara)
+gages2_polygons_not_cara = gages2_polygons_not_cara[
+    gages2_polygons_not_cara.geometry.bounds.miny > conus_bbox[0]
+]
+gages2_polygons_not_cara = gages2_polygons_not_cara[
+    gages2_polygons_not_cara.geometry.bounds.minx > conus_bbox[1]
+]
+gages2_polygons_not_cara = gages2_polygons_not_cara[
+    gages2_polygons_not_cara.geometry.bounds.maxy < conus_bbox[2]
+]
+gages2_polygons_not_cara = gages2_polygons_not_cara[
+    gages2_polygons_not_cara.geometry.bounds.maxx < conus_bbox[3]
+]
+print(
+    f"There are {len(gages2_polygons_not_cara)} Caravan+GAGES2 gauges after dropping {length_before - len(gages2_polygons_not_cara)} gauges not in CONUS based on lat/lon"
+)
+# %%
+gages2_polygons_not_cara.to_file(
+    os.path.join(gages2_dir, "gages2_polygons_not_cara.shp"), driver="ESRI Shapefile"
+)
+
+# %%
