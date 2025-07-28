25515d5bf1a8af99686a70fe214f1072deec5d7a
RY4GIT <400mhs2@gmail.com>
2025-07-28 12:35:14 -0700

    code to get hourly camels

--- signatures/test_sigEvent_CAMELShourly.m
+++ signatures/test_sigEvent_CAMELShourly.m
@@ -22,14 +22,15 @@ addpath(genpath(fullfile(baseDir, TOSSHDir)));
 % Define directories and file type
 cloud_dir = 'G:\Shared drives\Signatures -- large scale\baseflow\RAraki'; % 'G:\Araki' on lab computer
 data_dir = 'D:\data';
+hourly_dir = 'CAMELShourly';
 caravan_dir = 'Caravan1.5';
 attributes_dir = 'attributes';
 timeseries_dir = 'timeseries';
 data_type = 'csv';
 
 currentDate = datestr(now, 'yyyymmdd');
-out_dir = fullfile(cloud_dir, 'out', 'signatures', ['caravan_', caravan_data, '_', currentDate]);
-out_filename = ['out_' sig_cat '.csv'];
+out_dir = fullfile(cloud_dir, 'out', 'signatures', ['test_hourlyCAMELS_', currentDate]);
+out_filename = 'out_sigEvent.csv';
 if ~exist(out_dir, 'dir')
     mkdir(out_dir);  % This will create the directory and any necessary subdirectories
     fprintf('Directory created: %s\n', out_dir);
@@ -39,7 +40,7 @@ end
 
 %___________________________________________________________________________________
 % Read metadata
-attrs_geo = readtable(fullfile(data_dir, caravan_dir, attributes_dir, caravan_data, ['attributes_other_' caravan_data '.' data_type]));
+attrs_geo = readtable(fullfile(data_dir, caravan_dir, attributes_dir, "camels", "attributes_other_camels.csv"));
 attrs_geo_names = attrs_geo.Properties.VariableNames;
 % disp(head(attrs_geo));
 
@@ -53,11 +54,15 @@ numGauges = height(us_gauges);
 % Parameter config
 config_OF = readtable('config_overlandflow.csv');
 config_recession = readtable('config_recession.csv');
-
 plot_results = false;
 
-%___________________________________________________________________________________
-% Prepare parallel pool
+% Initialize
+resultsCell = cell(numGauges, 1);
+
+
+%
+% %___________________________________________________________________________________
+% % Prepare parallel pool
 %
 % Specify the number of workers
 numWorkers = 6;  % Adjust based on your system capabilities
@@ -68,144 +73,128 @@ if isempty(pool)
     parpool(numWorkers);  % Start a parallel pool
 end
 
-% Initialize the cell array for results
-resultsCell = cell(numGauges, 1);
-
 % Progress update setup
-fprintf("Starting processing ... %s dataset", caravan_data);
+fprintf("Starting processing hourly CAMELS dataset");
 
 %___________________________________________________________________________________
 % Loop through each gauge in us_gauges and collect data
-for idx = 1:numGauges
+parfor idx = 1:numGauges
     try
         % Get the gauge id
         gauge_id = cell2mat(us_gauges(idx, :).gauge_id);
         fprintf("Currently processing %s\n", gauge_id)
-        
+
+        % Get the gauge number
+        gauge_num = extractAfter(gauge_id, '_');
+        fprintf("Gauge number: %s\n", gauge_num)
+
+
+
         %___________________________________________________________________________________
         % Data preparation
+
         % Load data and convert it to datetime table
-        file_path = fullfile(data_dir, caravan_dir, timeseries_dir,data_type, caravan_data, [gauge_id '.' data_type]);
-        data = readtable(file_path);
-        data.date = datetime(data.date, 'InputFormat', 'yyyy-MM-dd');
-        data_timetable = table2timetable(data, 'RowTimes', 'date');
+        try
+            q_file_path = fullfile(data_dir, hourly_dir, "usgs_streamflow", [gauge_num '-usgs-hourly.' data_type]);
+            q_data = readtable(q_file_path);
+        catch
+            fprintf("No USGS data found for %s\n", gauge_id)
+            continue
+        end
+
+        % Streamflow data
+        q_data.date = datetime(q_data.date, 'InputFormat', 'yyyy-MM-dd');
+        q_tt = table2timetable(q_data, 'RowTimes', 'date');
         %     disp(head(data_timetable));
-        
+        % plot(q_tt.QObs_mm_h_)
+
+        % NLDAS forcing
+        nldas_file_path = fullfile(data_dir, hourly_dir, "nldas_hourly", [gauge_num '_hourly_nldas.' data_type]);
+        nldas_data = readtable(nldas_file_path);
+        nldas_data.date = datetime(nldas_data.date, 'InputFormat', 'yyyy-MM-dd');
+        nldas_tt = table2timetable(nldas_data, 'RowTimes', 'date');
+
+        % Synchronize the timetables using NLDAS as temporal baseline
+        data = synchronize(q_tt, nldas_tt, 'union', 'fillwithmissing');
+
         % Prepare TOSSH imput
-        Q = num2cell(data.streamflow,1); %mm/day
+        Q = num2cell(data.QObs_mm_h_,1); %mm/day
         t = num2cell(data.date,1);
-        P = num2cell(data.total_precipitation_sum,1);
-        PET = num2cell(data.potential_evaporation_sum_FAO_PENMAN_MONTEITH,1);
-        T = num2cell(data.temperature_2m_mean,1);
-        
+        P = num2cell(data.total_precipitation,1);
+        PET = num2cell(data.potential_evaporation,1);
+        T = num2cell(data.temperature,1);
+
         %___________________________________________________________________________________
         % Get parameters
-        
-        if strcmp(sig_cat,'calc_All_custom')
-            
-            try
-                % Overland flow
-                parts = split(gauge_id, '_');
-                gauge_code = parts{2};
-                ws_code = str2double(gauge_code(1:2));
-                OF_param = config_OF(config_OF.ws_code == ws_code, :);
-                
-                % Recession
-                p95 = prctile(data.streamflow, 95);
-                if (p95 < 1)
-                    recession_param = config_recession(string(config_recession.flow) == {'low'}, :);
-                else
-                    recession_param = config_recession(string(config_recession.flow) == {'normal'}, :);
-                end
-                
-            catch ME
-                fprintf('Error at index %d: %s\n', idx, ME.message);
-            end
-        end
-        
+
+        % Overland flow
+        ws_code = str2double(gauge_num(1:2));
+        OF_param = config_OF(config_OF.ws_code == ws_code, :);
+
+
         %___________________________________________________________________________________
-        % Signature calculation
-        switch sig_cat
-            config_OF = readtable('config_overlandflow.csv');
+        % Event separation & IE SE signatures
+        IE_effect = NaN(size(Q,1),1);
+        SE_effect = NaN(size(Q,1),1);
+        IE_thresh_signif = NaN(size(Q,1),1);
+        IE_thresh = NaN(size(Q,1),1);
+        SE_thresh_signif = NaN(size(Q,1),1);
+        SE_thresh = NaN(size(Q,1),1);
+        SE_slope = NaN(size(Q,1),1);
+        Storage_thresh_signif = NaN(size(Q,1),1);
+        Storage_thresh = NaN(size(Q,1),1);
+        min_Qf_perc = NaN(size(Q,1),1);
+        R_Pvol_RC = NaN(size(Q,1),1);
+        R_Pint_RC = NaN(size(Q,1),1);
+        OF_error_str = strings(size(Q,1),1);
 
-parts = split(gauge_id, '_');
-gauge_code = parts{2};
-ws_code = str2double(gauge_code(1:2));
-OF_param = config_OF(config_OF.ws_code == ws_code, :);
+        [IE_effect,SE_effect,IE_thresh_signif,IE_thresh, ...
+            SE_thresh_signif,SE_thresh,SE_slope,Storage_thresh, ...
+            Storage_thresh_signif,min_Qf_perc,R_Pvol_RC, R_Pint_RC,~,OF_error_str] ...
+            = sig_EventGraphThresholds(Q{1},t{1},P{1},...
+            'min_termination', OF_param.min_termination, ...
+            'min_duration', OF_param.min_duration, ...
+            'min_intensity_day', OF_param.min_intensity_day, ...
+            'min_intensity_day_during', OF_param.min_intensity_day_during, ...
+            'max_recessiondays', OF_param.max_recessiondays, ...
+            'plot_results',plot_results);
 
+        results = [];
+        results.gauge_id = gauge_id;
+        results.IE_effect = IE_effect;
+        results.SE_effect = SE_effect;
+        results.IE_thresh_signif = IE_thresh_signif;
+        results.SE_thresh_signif = SE_thresh_signif;
+        results.IE_thresh = IE_thresh;
+        results.SE_thresh = SE_thresh;
+        results.SE_slope = SE_slope;
+        results.Storage_thresh_signif = Storage_thresh_signif;
+        results.Storage_thresh = Storage_thresh;
+        results.min_Qf_perc = min_Qf_perc;
+        results.R_Pvol_RC = R_Pvol_RC;
+        results.R_Pint_RC = R_Pint_RC;
+        results.OF_error_str = OF_error_str;
 
-timestep = 24; % time step of precipitation array [hours] (1=hourly, 24=daily)
-
-min_termination = OF_param.min_termination; % 48; % minimum termination time (time between storms) [hours]
-
-min_intensity_day = OF_param.min_intensity_day; % 4.8; % minimum intensity (per day)
-min_intensity_day_during = OF_param.min_intensity_day_during; % 4.8; % minimum timestep intensity allowed during storm event without contributing to termination time
-
-min_duration = OF_param.min_duration; % 24; % minimum duration of storm [hours]
-
-max_recessiondays = OF_param.max_recessiondays; % 8; % maximum number of days to allow recession after rain ends
-
-min_intensity_hour = 2; % minimum intensity (per hour)
-min_intensity_hour_during = 0.2; % minimum timestep intensity allowed during storm event without contributing to termination time
-
-plot_results = true;
-
-%___________________________________________________________________________________
-% Event separation & IE SE signatures
-
-[IE_effect, SE_effect, IE_thresh_signif, IE_thresh, ...
-    SE_thresh_signif, SE_thresh, SE_slope, ...
-    Storage_thresh, Storage_thresh_signif, min_Qf_perc, ...
-     R_Pvol_RC, R_Pint_RC, fig_event] = sig_EventGraphThresholds(Q,t,P,...
-    'min_termination', min_termination, ...
-    'min_duration', min_duration, ...
-    'min_intensity_day', min_intensity_day, ...
-    'min_intensity_day_during', min_intensity_day_during, ...
-    'max_recessiondays', max_recessiondays, ...
-    'plot_results', plot_results);
-        end
-        
         % Make table
-        signatures = struct2table(signatures);
+        signatures = struct2table(results);
 %         signatures.gauge_id = gauge_id; % this somehow doesn't work when
 %         doing vertcat
         
         % Store the results in the Composite variable
         resultsCell{idx} = signatures;
-        
+
     catch ME
         fprintf('Error at index %d: %s\n', idx, ME.message);
 
         % Create an empty output
         fieldNames = {
-            'AC1', 'AC1_error_str', 'BaseflowRecessionK', 'BaseflowRecessionK_error_str', ...
-            'BaseflowMagnitude', 'BaseflowMagnitude_error_str', 'BFI', 'BFI_error_str', ...
-            'EventGraphThresholds', 'EventGraphThresholds_error_str', 'EventRR', 'EventRR_error_str', ...
-            'FDC', 'FDC_error_str', 'FDC_slope', 'FDC_slope_error_str', ...
-            'FlashinessIndex', 'FlashinessIndex_error_str', 'HFD_mean', 'HFD_mean_error_str', ...
-            'HFI_mean', 'HFI_mean_error_str', 'MRC_SlopeChanges', 'MRC_SlopeChanges_error_str', ...
-            'PeakDistribution', 'PeakDistribution_error_str', 'PQ_Curve', 'PQ_Curve_error_str', ...
-            'Q_CoV', 'Q_CoV_error_str', 'Q_mean', 'Q_mean_error_str', 'Q_mean_monthly', ...
-            'Q_mean_monthly_error_str', 'Q_7_day_max', 'Q_7_day_max_error_str', 'Q_7_day_min', ...
-            'Q_7_day_min_error_str', 'Q_skew', 'Q_skew_error_str', 'Q_var', 'Q_var_error_str', ...
-            'QP_elasticity', 'QP_elasticity_error_str', 'RecessionParameters_a', 'RecessionParameters_b', 'RecessionParameters_T0', 'RecessionParameters_error_str', ...
-            'RecessionK_early', 'RecessionK_early_error_str', 'Spearmans_rho', 'Spearmans_rho_error_str', ...
-            'ResponseTime', 'ResponseTime_error_str', 'RLD', 'RLD_error_str', 'RR_Seasonality', ...
-            'RR_Seasonality_error_str', 'SeasonalTranslation', 'SeasonalTranslation_error_str', ...
-            'Recession_a_Seasonality', 'Recession_a_Seasonality_error_str', 'SnowDayRatio', ...
-            'SnowDayRatio_error_str', 'SnowStorage', 'SnowStorage_error_str', 'StorageFraction', ...
-            'StorageFraction_error_str', 'StorageFromBaseflow', 'StorageFromBaseflow_error_str', ...
-            'TotalRR', 'TotalRR_error_str', 'VariabilityIndex', 'VariabilityIndex_error_str', ...
-            'Q95', 'Q95_error_str', 'high_Q_duration', 'high_Q_duration_error_str', ...
-            'high_Q_frequency', 'high_Q_frequency_error_str', 'IE_effect', 'SE_effect', ...
+            'IE_effect', 'SE_effect', ...
             'IE_thresh_signif', 'SE_thresh_signif', 'IE_thresh', 'SE_thresh', 'SE_slope', ...
-            'Storage_thresh_signif', 'Storage_thresh', 'min_Qf_perc', 'R_Pvol_RC', 'R_Pint_RC', 'OF_error_str', ...
-            'AverageStorage', 'MRC_num_segments', 'MRC_num_segments_error_str', ...
-            'First_Recession_Slope', 'Mid_Recession_Slope', 'EventRR_TotalRR_ratio'
-        };
-        
+            'Storage_thresh_signif', 'Storage_thresh', 'min_Qf_perc', 'R_Pvol_RC', 'R_Pint_RC', 'OF_error_str'
+            };
+
         % Initialize the struct dynamically
-        signatures = struct(); 
+        signatures = struct();
         for i = 1:numel(fieldNames)
             if contains(fieldNames{i}, '_error_str')  % If field is an error string field
                 signatures.(fieldNames{i}) = "";         % Assign empty string
@@ -215,10 +204,10 @@ plot_results = true;
                 signatures.(fieldNames{i}) = {NaN(1, 2), NaN(1,2)};
             elseif strcmp(fieldNames{i}, 'PQ_Curve') % Ensure MRC_SlopeChanges is an array of length 2
                 signatures.(fieldNames{i}) = NaN(1, 4);
-            % elseif strcmp(fieldNames{i}, 'RecessionParameters') % Ensure
-            % MRC_SlopeChanges is an array of length 2 ... when the first
-            % is for entire (non-individual) flow duration curve
-            %     signatures.(fieldNames{i}) = NaN(1, 2);
+                % elseif strcmp(fieldNames{i}, 'RecessionParameters') % Ensure
+                % MRC_SlopeChanges is an array of length 2 ... when the first
+                % is for entire (non-individual) flow duration curve
+                %     signatures.(fieldNames{i}) = NaN(1, 2);
             elseif strcmp(fieldNames{i}, 'SeasonalTranslation') % Ensure MRC_SlopeChanges is an array of length 2
                 signatures.(fieldNames{i}) = NaN(1, 2);
             elseif strcmp(fieldNames{i}, 'StorageFraction') % Ensure MRC_SlopeChanges is an array of length 2
@@ -227,7 +216,7 @@ plot_results = true;
                 signatures.(fieldNames{i}) = NaN(1, 1);        % Assign NaN for numerical values
             end
         end
-        
+        signatures.gauge_id = gauge_id;
         % Make table
         signatures = struct2table(signatures);
         resultsCell{idx} = signatures;
@@ -237,13 +226,6 @@ end
 
 % Combine all results into one table after the loop
 results = vertcat(resultsCell{:});
-results.gauge_id = us_gauges.gauge_id(1:numGauges);
-
-if contains(sig_cat, 'calc_All')
-    % remove FDC to save space
-    results.FDC = [];
-    results.FDC_error_str = [];
-end
 
 % Save the table to a CSV file
 writetable(results, fullfile(out_dir, out_filename), 'WriteVariableNames', true);
