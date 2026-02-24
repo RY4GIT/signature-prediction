e70fef9dc036573c7d2b21e0e3b8e34c81163b3b
RY4GIT <400mhs2@gmail.com>
2026-02-24 15:15:08 -0800

    time stability of signatures

--- figures/HESS25_R1/plot_sigs_timestability.py
+++ figures/HESS25_R1/plot_sigs_timestability.py
@@ -7,31 +7,71 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
-import seaborn as sns
+import random
 
-
+# %%
 home_dir = r"G:\Shared drives\Signatures -- large scale\baseflow\RAraki"
-data_dir = "data"
+data_dir = r"D:\data"
 sig_output_dir = r"out\signatures"
 caravan_dir = r"Caravan1.4"
 attributes_dir = "attributes"
 caravan_data = "camels"
-data_type="csv"
+data_type = "csv"
 fig_dir = "figs"
-plot_config = pd.read_csv("plot_sig_configs.csv", index_col=0)
 
-attrs_geo = pd.read_csv(os.path.join(home_dir, data_dir, caravan_dir, attributes_dir, caravan_data, f"attributes_other_{caravan_data}.{data_type}"))
+_plot_config = pd.read_csv(
+    "plot_sigs_config_timestability.csv",
+)
+
+# %% Make a shorter list of signatures to plot
+
+desired_columns = [
+    "BFI",
+    "BaseflowRecessionK",
+    "AverageStorage",
+    "RecessionParameters_2",
+    "TotalRR",
+    "EventRR",
+    "Recession_a_Seasonality",
+    "VariabilityIndex",
+    # "IE_thresh_signif",
+    # "SE_thresh_signif",
+    "IE_thresh",
+    "SE_thresh",
+]
+
+# Filter to only desired signatures and order by the specified list
+plot_config = _plot_config[_plot_config["column_name"].isin(desired_columns)].copy()
+plot_config["__order"] = pd.Categorical(
+    plot_config["column_name"], categories=desired_columns, ordered=True
+)
+plot_config = plot_config.sort_values("__order").drop(columns="__order")
+
+
+# %%
+
+attrs_geo = pd.read_csv(
+    os.path.join(
+        data_dir,
+        caravan_dir,
+        attributes_dir,
+        caravan_data,
+        f"attributes_other_{caravan_data}.{data_type}",
+    )
+)
 attrs_geo.set_index("gauge_id")
 attrs_geo_names = attrs_geo.columns.to_list()
 attrs_geo.head()
 
-gauge_ids = attrs_geo["gauge_id"][attrs_geo["gauge_id"].str.startswith(f'{caravan_data}_')].tolist()
-import random
-gauge_ids_rnd = random.sample(gauge_ids, 30)
-# caravan_data = "camels"
+gauge_ids = attrs_geo["gauge_id"][
+    attrs_geo["gauge_id"].str.startswith(f"{caravan_data}_")
+].tolist()
+
+gauge_ids_rnd = random.sample(gauge_ids, 10)
+
 # %%
 ########################################
-camels_results_dir = "caravan_camels_timestability_20240606" 
+camels_results_dir = "caravan_camels_timestability_20240606"
 sig_cat = "calc_ALL"  # 'calc_ALL', or 'McMillan set' 'calc_McMillan_OverlandFlow', 'calc_McMillan_Groundwater'
 ########################################
 
@@ -39,7 +79,7 @@ sig_cat = "calc_ALL"  # 'calc_ALL', or 'McMillan set' 'calc_McMillan_OverlandFlo
 num_plots = len(plot_config)
 num_rows = (num_plots + 3) // 4  # Ceiling division to ensure all plots fit
 # Number of rows for 4 plots per row
-plt.rcParams['font.size'] = 12
+plt.rcParams["font.size"] = 12
 fig, axes = plt.subplots(
     nrows=num_rows, ncols=4, figsize=(15, 2.5 * num_rows)
 )  # Adjust the size as needed
@@ -47,13 +87,19 @@ axes = axes.flatten()  # Flatten the axes array for easy iteration
 
 # ______________________________________________________________________________________________
 # Load data
-for i, gauge_id in enumerate(gauge_ids):
+for i, gauge_id in enumerate(
+    gauge_ids
+):  # Choose gauge_ids_rnd (smaller set) or gauge_ids (all gauges)
     # ______________________________________________________________________________________________
     # Get the data
 
-    # gauge_id = "camels_01013500"
     sigs = pd.read_csv(
-        os.path.join(home_dir, sig_output_dir, camels_results_dir, f"out_{sig_cat}_{gauge_id}.csv")
+        os.path.join(
+            home_dir,
+            sig_output_dir,
+            camels_results_dir,
+            f"out_{sig_cat}_{gauge_id}.csv",
+        )
     )
     sigs.set_index("gauge_id", inplace=True)
     sigs["duration"] = sigs["end_year"] - sigs["start_year"]
@@ -64,27 +110,31 @@ for i, gauge_id in enumerate(gauge_ids):
     # Plot each histogram
     for ax, (index, row) in zip(axes, plot_config.iterrows()):
         try:
-            ax.plot(sigs["duration"], sigs[row['column_name']],alpha=0.3, color="tab:blue")
+            ax.plot(
+                sigs["duration"], sigs[row["column_name"]], alpha=0.3, color="tab:blue"
+            )
 
-            if i==0:
+            if i == 0:
                 ax.set_ylabel(f"{row['label']} {row['unit']}")
                 # Set x-ticks every 5 units, turn off labels for intermediate ticks
                 x_ticks = np.arange(0, max(sigs["duration"]) + 5, 5)
                 ax.set_xlabel("Duration (year)")
                 ax.set_xticks(x_ticks)  # Set major ticks every 5
-                # ax.set_ylim([row["lower_lim"], row["upper_lim"]])
+                ax.set_ylim([row["lower_lim"], row["upper_lim"]])
                 # ax.set_xticklabels([str(tick) if (tick % 2.5 == 0) else "" for tick in x_ticks])
-        except:
+        except Exception:
             continue
 
-    if i==0:
+    if i == 0:
         # Disable unused axes if any
         for j in range(num_plots, len(axes)):
             axes[j].axis("off")
-            
+
             # TODO: fix this
             # Check if this is the last plot to add a legend
-            if j == num_plots-1:  # Adjust the condition based on your total number of subplots
+            if (
+                j == num_plots - 1
+            ):  # Adjust the condition based on your total number of subplots
                 axes[j].legend()
 
     del sigs
@@ -93,6 +143,10 @@ for i, gauge_id in enumerate(gauge_ids):
 plt.tight_layout()
 plt.show()
 # %%
-fig.savefig(os.path.join(home_dir, sig_output_dir, camels_results_dir, f"sig_vs_duration.png"))
+fig.savefig(
+    os.path.join(
+        home_dir, sig_output_dir, camels_results_dir, "sig_vs_duration_selected.png"
+    )
+)
 
 # %%
